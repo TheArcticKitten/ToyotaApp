addd335e5c2d09c82d26a45a8c2234809bcb1b02
MercurialAnt <mata.anthony98@gmail.com>
2017-04-22 12:30:13 -0500

    Added pre lab in order tto spawn multiple enemies

---
+++ Toyota Driving Game/Assets/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+
+    public GameObject Diamond;
+    public float  maxPos= 23,  minPos= -23;
+    public float delayTimer = 1f;
+    float timer;
+    // Use this for initialization
+    void Start () {
+        timer = delayTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer -= Time.deltaTime;
+        if(timer<=0)
+        {
+            Vector3 diamondPos = new Vector3(Random.Range(minPos, maxPos), transform.position.y, transform.position.z);
+            Instantiate(Diamond, diamondPos, transform.rotation);
+            timer = delayTimer;
+        }
+    }
+}
