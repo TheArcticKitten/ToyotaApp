0905f8583546d20dbd4bf8e9f1f4cea77a6d5a0e
TheArcticKitten <noahortega58@gmail.com>
2017-04-22 19:46:47 -0500

    Added main Menu

---
+++ Toyota Driving Game/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+    public float velX, velY;
+    public bool collide;
+    //public GameObject diamond;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(velX, velY, 0));
+        //if (transform.position.y <= -16.5) Destroy(gameObject);
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collide && collision.gameObject.tag == "Character") Destroy(gameObject);    
+    }
+}
