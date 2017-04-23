2a20186251c4d7d3ec219815a105f0226e488240
TheArcticKitten <noahortega58@gmail.com>
2017-04-23 01:02:53 -0500

    Broken score and health scaling

---
+++ Toyota Driving Game/Assets/Scripts/score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour {
+    public Text mygui;
+    int counter = 0, counter2 = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        counter++;
+        if(counter% 30 ==0)
+        {
+            mygui.text = counter2++ + "";
+        }
+	}
+}
